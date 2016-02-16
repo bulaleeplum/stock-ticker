8ae0c17ab17dac1cbbb336f5b22754587e6e7c11
Spenser Lee <Spenser.L159@gmail.com>
2016-02-16 04:26:07 -0800

    updated template page with materialize stlying

--- application/views/stock_history.php
+++ application/views/stock_history.php
@@ -1,9 +1,13 @@
-<select>
-    <option>Option 1</option>
-    <option>Option 2</option>
-    <option>Option 3</option>
-</select>
-<table>
+<div class="input-field col s12">
+    <select>
+        <option value="" disabled selected>Choose your option</option>
+        <option value="1">Option 1</option>
+        <option value="2">Option 2</option>
+        <option value="3">Option 3</option>
+    </select>
+    <label>Stock Options</label>
+</div>
+<table class="highlight">
     <tr>
         <th>Stock movement</th>
         <th>Buy/Sell</th>

ca9c2cc18e493878d3d7abc052ea7e59d3512541
Scott-Plummer <Scott.Plummer18@gmail.com>
2016-02-15 22:17:24 -0800

    Added a basic template table

--- application/views/stock_history.php
+++ application/views/stock_history.php
@@ -1,7 +1,15 @@
-<?php
-    /**
-     * Created by PhpStorm.
-     * User: scott
-     * Date: 15/02/16
-     * Time: 3:08 PM
-     */
+<select>
+    <option>Option 1</option>
+    <option>Option 2</option>
+    <option>Option 3</option>
+</select>
+<table>
+    <tr>
+        <th>Stock movement</th>
+        <th>Buy/Sell</th>
+    </tr>
+    <tr>
+        <td>Dummy data1</td>
+        <td>Dummy data2</td>
+    </tr>
+</table>

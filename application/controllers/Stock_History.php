afc039e869006eda4017e9e1c3ce2a491095ad76
Scott-Plummer <Scott.Plummer18@gmail.com>
2016-02-18 12:47:28 -0800

    Generated stock table

--- application/controllers/Stock_History.php
+++ application/controllers/Stock_History.php
@@ -12,9 +12,27 @@ class Stock_History extends MY_Controller {
         parent::__construct();
     }
 
+    /*
+     * Display screen contents
+     */
     function index() {
+        $this->load->model('StocksModel');
+        $this->load->library('table');
+
         $this->data['pagetitle'] = 'Stock History';
         $this->data['pagebody'] = 'stock_history';
+        $value = $this->input->post('stock_select');
+
+        $this->table->set_heading('Code', 'Date', 'Stock Movement',
+            'Quantity', 'Transaction');
+
+        if(!empty($value)) {
+            $action = $this->StocksModel->getStockHistory($value);
+            foreach ($action as $row) {
+                $this->table->add_row($row);
+            }
+        }
+        $this->data['table'] = $this->table->generate();
         $this->render();
     }
 }
--- application/models/StocksModel.php
+++ application/models/StocksModel.php
@@ -24,6 +24,37 @@ class StocksModel extends CI_Model {
     }
 
     /**
+     * Retrieve the stock history of a given stock
+     * @param $stockCode The code name for the stock to retrieve from the
+     * database
+     * @return mixed The array of results from the query
+     */
+    function getStockHistory($stockCode) {
+        $this->db->select('stocks.Code, movements.DateTime, movements.Action,
+                            transactions.Quantity, transactions.Trans');
+        $this->db->from('stocks');
+        $this->db->join('movements', 'stocks.code = movements.code');
+        $this->db->join('transactions', 'movements.code = transactions.stock');
+        $this->db->where('stocks.code = ', $stockCode);
+        $query = $this->db->get();
+
+        return $query->result_array();
+    }
+
+    /**
+     * Retrieve the all the stock names and their codes in the database
+     * @return mixed The array of names and codes.
+     */
+    function getStockNamesAndCodes() {
+        $this->db->select('stocks.Name, stocks.Code');
+        $this->db->from('stocks');
+
+        $query = $this->db->get();
+
+        return $query->result_array();
+    }
+
+    /**
      * Gets the stocks table.
      */
     function getStocks() {

ffd6f0203bc78f14553763e9b9447037aaeae29f
Scott-Plummer <Scott.Plummer18@gmail.com>
2016-02-19 15:35:02 -0800

    Implemented stock-history routing

--- application/views/stock_history.php
+++ application/views/stock_history.php
@@ -1,5 +1,5 @@
 <div class="input-field col s3">
-    <form method="post">
+    <form method="GET" id="stock-form" action="/Stock_History/getSpecificStock">
         <select name="stock_select" onchange="this.form.submit()">
             <option value="" disabled selected>Choose your option</option>
             <option value="BOND">BOND</option>

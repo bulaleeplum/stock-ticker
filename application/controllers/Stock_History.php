6254391ba1208bcf0ca9e6715aded1d3cda7fafc
Scott-Plummer <Scott.Plummer18@gmail.com>
2016-02-19 15:44:14 -0800

    Fixed renaming stockhistory model

--- application/controllers/Stock_History.php
+++ application/controllers/Stock_History.php
@@ -21,8 +21,8 @@ class Stock_History extends MY_Controller {
 
         $this->data['pagetitle'] = 'Stock History';
         $this->data['pagebody'] = 'stock_history';
-        $mostRecent = $this->StocksModel->getMostRecentStock();
-        $stockHistory = $this->StocksModel->getStockHistory($mostRecent[0]['code']);
+        $mostRecent = $this->StockHistory->getMostRecentStock();
+        $stockHistory = $this->StockHistory->getStockHistory($mostRecent[0]['code']);
         $this->data['table'] = $this->generateTable($stockHistory);
 
         $this->render();
@@ -32,7 +32,7 @@ class Stock_History extends MY_Controller {
         $this->load->library('table');
         $this->data['pagetitle'] = 'Stock History';
         $this->data['pagebody'] = 'stock_history';
-        $stockHistory = $this->StocksModel->getStockHistory($stock);
+        $stockHistory = $this->StockHistory->getStockHistory($stock);
         $this->data['table'] = $this->generateTable($stockHistory);
 
         $this->render();

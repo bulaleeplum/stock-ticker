cfff7728eb591f23fc252403cd2799d193e19f3c
Scott-Plummer <Scott.Plummer18@gmail.com>
2016-02-19 16:24:38 -0800

    Generate stock dropdown

--- application/controllers/Stock_History.php
+++ application/controllers/Stock_History.php
@@ -14,13 +14,14 @@ class Stock_History extends MY_Controller {
     }
 
     /**
-     * Generates the stock history page values and passes them to the view
+     * Generates the default stock history page with
      */
     function index() {
         $this->load->library('table');
 
         $this->data['pagetitle'] = 'Stock History';
         $this->data['pagebody'] = 'stock_history';
+        $this->data['stocks'] = $this->generateDropdown();
         $mostRecent = $this->StockHistory->getMostRecentStock();
         $stockHistory = $this->StockHistory->getStockHistory($mostRecent[0]['code']);
         $this->data['table'] = $this->generateTable($stockHistory);
@@ -28,16 +29,24 @@ class Stock_History extends MY_Controller {
         $this->render();
     }
 
+    /**
+     * Displays a given stock history information into the table
+     * @param $stock The stock to display the history about
+     */
     function displayStock($stock) {
         $this->load->library('table');
         $this->data['pagetitle'] = 'Stock History';
         $this->data['pagebody'] = 'stock_history';
+        $this->data['stocks'] = $this->generateDropdown();
         $stockHistory = $this->StockHistory->getStockHistory($stock);
         $this->data['table'] = $this->generateTable($stockHistory);
 
         $this->render();
     }
 
+    /**
+     * Routing function for the specific stock
+     */
     function getSpecificStock() {
         $stock = $this->input->get('stock_select');
         redirect("stock-history/$stock");
@@ -50,6 +59,10 @@ class Stock_History extends MY_Controller {
      * @return mixed The generated table
      */
     function generateTable($stock) {
+        //sets the table style
+        $template = array('table_open' => '<table class="highlight">');
+        $this->table->set_template($template);
+
         $this->table->set_heading('Code', 'Date', 'Stock Movement',
             'Quantity', 'Transaction');
         foreach ($stock as $row) {
@@ -57,5 +70,19 @@ class Stock_History extends MY_Controller {
         }
 
         return $this->table->generate();
+    }
+
+    /**
+     * Generates the drop down of stock values
+     * @return string The html dropdown
+     */
+    function generateDropdown() {
+        $options = "";
+        $players = $this->StockHistory->getStocks();
+        foreach ($players as $row) {
+            $options .= "<option value=" . $row['code'] . ">" . $row['name'] .
+                "</option>";
+        }
+        return $options;
     }
 }

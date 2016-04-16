e7c6d61cd5913b83f513629f5f314956dad3ac0f
Scott-Plummer <Scott.Plummer18@gmail.com>
2016-04-15 23:10:44 -0700

    Added insertion into database from server code

--- application/models/GameModel.php
+++ application/models/GameModel.php
@@ -5,8 +5,6 @@
      * Date: 15/04/16
      * Time: 2:16 PM
      */
-define('SERVER_BACKUP', ('http://www.comp4711bsx.local/data/'));
-define('SERVER', ('http://bsx.jlparry.com/data/'));
     class GameModel extends CI_Model {
 
         /**
@@ -16,44 +14,4 @@ define('SERVER', ('http://bsx.jlparry.com/data/'));
             parent::__construct();
         }
 
-
-        function getStocks() {
-            return $this->importCSV2Array(SERVER . 'stocks', 'r');
-        }
-
-        function getMovements() {
-            return $this->importCSV2Array(SERVER . 'movement', 'r');
-        }
-
-        function getTransactions() {
-            return $this->importCSV2Array(SERVER . 'transactions', 'r');
-        }
-
-        private function importCSV2Array($filename) {
-            $row = 0;
-            $col = 0;
-            $handle = @fopen($filename, "r");
-            if ($handle) {
-                while (($row = fgetcsv($handle, 4096)) !== false) {
-                    if (empty($fields)) {
-                        $fields = $row;
-                        continue;
-                    }
-
-                    foreach ($row as $k => $value) {
-                        $results[$col][$fields[$k]] = $value;
-                    }
-                    $col++;
-                    unset($row);
-                }
-                if (!feof($handle)) {
-                    echo "Error: unexpected fgets() failng";
-                }
-                fclose($handle);
-            } else {
-                echo "fopen failed";
-            }
-
-            return isset($results) ? $results : "";
-        }
     }
